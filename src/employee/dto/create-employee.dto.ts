9841981e971f81fb9734bacab8f98075b999a055
Sachin <sachinlakshan04@gmail.com>
2025-06-20 23:54:38 +0530

    validate input with class-validator

--- src/employee/dto/create-employee.dto.ts
+++ src/employee/dto/create-employee.dto.ts
@@ -1,1 +1,27 @@
-export class CreateEmployeeDto {}
+import { IsEmail, IsEnum, IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { Department, Designation } from '../enums/employee.enums';
+
+export class CreateEmployeeDto {
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsEnum(Designation)
+  @IsNotEmpty()
+  designation: Designation;
+
+  @IsEnum(Department)
+  @IsNotEmpty()
+  department: Department;
+
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
+  salary: number;
+}
